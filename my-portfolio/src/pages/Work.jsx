dc9fdcb627c1962e3a3f699e5bebd79c9aac0380
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-15 10:22:26 -0500

    I adjusted the projects component to map into the array of objects

--- my-portfolio/src/pages/Work.jsx
+++ my-portfolio/src/pages/Work.jsx
@@ -8,8 +8,16 @@ import note_takerIMG from '../assets/note_taker.png'
 
 const projects = [
   { img: pwaIMG, githubLink: "https://github.com/rodolfoespinosa01/bc_c19_pwa", demoLink: "https://warm-sierra-95511-1e709179fab2.herokuapp.com/", projectTitle: "Text Editor (pwa)" },
+
   { img: mvcIMG, githubLink: "https://github.com/rodolfoespinosa01/bc_c14_mvc", demoLink: "https://apohl53.github.io/Workout_Planner_Project/", projectTitle: "Workout Planner" },
+
   { img: workout_plannerIMG, githubLink: "https://github.com/rodolfoespinosa01/bc_c14_mvc", demoLink: "https://github.com/rodolfoespinosa01/Workout_Planner_Project", projectTitle: "Workout Planner" },
+
+  { img: quizIMG, githubLink: "https://github.com/rodolfoespinosa01/bc_c4_quizChallenge/", demoLink: "https://rodolfoespinosa01.github.io/bc_c4_quizChallenge/", projectTitle: "Single Page/App Quiz" },
+
+  { img: save_plannerIMG, githubLink: "https://github.com/CoreyPhillipsOfficial/save-smart", demoLink: "https://mighty-reaches-45633-a7b714f2317a.herokuapp.com/", projectTitle: "Save Smart Planner" },
+
+  { img: note_takerIMG, githubLink: "https://github.com/rodolfoespinosa01/bc_c11_express", demoLink: "https://still-depths-93922-76459986b917.herokuapp.com/", projectTitle: "Save Smart Planner" }
 
 ]
 
@@ -26,7 +34,7 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
-          {/* Project# 1&2 PWA*/}
+          {/* All Projects*/}
           {projects.map((project) => (
             <div style={{ backgroundImage: `url(${project.img})` }}
               className="shadow-lg shadow-[#00a6fb] group container rounded-md flex justify-center items-center mx-auto content-div">
@@ -44,83 +52,7 @@ const Work = () => {
               </div>
             </div>
           ))}
-          <div style={{ backgroundImage: `url(${mvcIMG})` }}
-            className="shadow-lg shadow-[#00a6fb] group container rounded-md flex justify-center items-center mx-auto content-div">
 
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Model View Controller - MYSQL
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://guarded-lowlands-94580-887effed5c7f.herokuapp.com/"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button></a>
-                <a href="https://github.com/rodolfoespinosa01/bc_c14_mvc"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button></a>
-
-              </div>
-            </div>
-          </div>
-          {/* Project# 3&4  */}
-          <div style={{ backgroundImage: `url(${workout_plannerIMG})` }}
-            className="shadow-lg shadow-[#00a6fb] group container rounded-md flex justify-center items-center mx-auto content-div">
-
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Workout Planner
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://apohl53.github.io/Workout_Planner_Project/"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button></a>
-                <a href="https://github.com/rodolfoespinosa01/Workout_Planner_Project"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button></a>
-
-              </div>
-            </div>
-          </div>
-          <div style={{ backgroundImage: `url(${quizIMG})` }}
-            className="shadow-lg shadow-[#00a6fb] group container rounded-md flex justify-center items-center mx-auto content-div">
-
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Javascript Quiz
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://github.com/rodolfoespinosa01/bc_c4_quizChallenge/"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button></a>
-                <a href="https://github.com/rodolfoespinosa01/bc_c4_quizChallenge/"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button></a>
-
-              </div>
-            </div>
-          </div>
-          {/* Project# 5&6 */}
-          <div style={{ backgroundImage: `url(${save_plannerIMG})` }}
-            className="shadow-lg shadow-[#00a6fb] group container rounded-md flex justify-center items-center mx-auto content-div">
-
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                $ave $mart
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://mighty-reaches-45633-a7b714f2317a.herokuapp.com/"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button></a>
-                <a href="https://github.com/CoreyPhillipsOfficial/save-smart"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button></a>
-
-              </div>
-            </div>
-          </div>
-          <div style={{ backgroundImage: `url(${note_takerIMG})` }}
-            className="shadow-lg shadow-[#00a6fb] group container rounded-md flex justify-center items-center mx-auto content-div">
-
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Local Storage CRUD
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://still-depths-93922-76459986b917.herokuapp.com/"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button></a>
-                <a href="https://github.com/rodolfoespinosa01/bc_c11_express"><button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button></a>
-
-              </div>
-            </div>
-          </div>
         </div>
 
 
